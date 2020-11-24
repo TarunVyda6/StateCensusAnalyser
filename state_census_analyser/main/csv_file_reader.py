e08ef723e6eae3af6813f55bf68f206ae8b48e4f
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-24 18:26:23 +0530

    [Tarun] Refactoredd : Code to avoid Dry violation

---
+++ state_census_analyser/main/csv_file_reader.py
@@ -0,0 +1,42 @@
+import csv
+import logging
+from state_census_analyser.main.csv_exceptions import *
+
+
+class CSVFileReader:
+    @staticmethod
+    def check_for_delimiter(path):
+        with open(path, newline="") as csv_data:
+            try:
+                csv.Sniffer().sniff(csv_data.read(), delimiters=",")
+            except Exception:
+                logging.debug("invalid delimiter")
+                raise WrongDelimiterException("invalid delimter")
+
+    @staticmethod
+    def check_for_header(path):
+        with open(path, mode='r', newline='') as csv_file:
+            if not csv.Sniffer().has_header(csv_file.read()):
+                raise WrongHeaderException("incorrect header")
+
+    @staticmethod
+    def load_csv_file(path):
+        if not path.endswith(".csv"):
+            logging.debug("Wrong file extension")
+            raise WrongFileType("wrong file extension")
+
+        try:
+            with open(path, mode='r', newline='') as csv_file:
+                CSVFileReader.check_for_delimiter(path)
+                CSVFileReader.check_for_header(path)
+                csv_reader = csv.DictReader(csv_file, delimiter=',')
+                census_list = []
+                for row in csv_reader:
+                    census_dictionary = {}
+                    for key in row:
+                        census_dictionary[key] = row.get(key)
+                    census_list.append(census_dictionary)
+                return census_list
+        except FileNotFoundError:
+            logging.debug("invalid file path")
+            raise FileNotPresent("invalid file path")
