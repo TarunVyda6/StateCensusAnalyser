471764058b0a2227b66d88cc4a1fc1aa476731b9
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-24 18:37:56 +0530

    [Tarun] Refactoredd : Code to avoid Dry violation

--- state_census_analyser/main/state_census_analyse.py
+++ state_census_analyser/main/state_census_analyse.py
@@ -30,3 +30,14 @@ class StateCensusAnalyser:
         census_list = CSVFileReader.load_csv_file(path)
         sorted_list = sorted(census_list, key=lambda x: x.get("State"), reverse=False)
         return json.dumps(sorted_list)
+
+    @staticmethod
+    def sort_by_state_population_descending(path):
+        """
+        takes csv file path as input and load the csv data and return list of data sorted by state population in json format
+        :param path: takes csv file path as param
+        :return: list of csv state code analyser data in sorted order by state population
+        """
+        census_list = CSVFileReader.load_csv_file(path)
+        sorted_list = sorted(census_list, key=lambda x: int(x.get("Population")), reverse=True)
+        return json.dumps(sorted_list)
--- state_census_analyser/test/test_state_census_analyser.py
+++ state_census_analyser/test/test_state_census_analyser.py
@@ -20,6 +20,16 @@ def test_for_matching_sort_by_states(path, result1, result2):
     assert json_list[len(json_list) - 1].get("State") == result2
 
 
+@pytest.mark.parametrize('path, result1, result2',
+                         [("IndiaStateCensusData - IndiaStateCensusData.csv", "Uttar Pradesh", "Sikkim"),
+                          ])
+def test_for_matching_sort_by_states(path, result1, result2):
+    path = os.path.join(os.path.dirname(__file__), path)
+    json_list = json.loads(StateCensusAnalyser.sort_by_state_population_descending(path))
+    assert json_list[0].get("State") == result1
+    assert json_list[len(json_list) - 1].get("State") == result2
+
+
 @pytest.mark.parametrize('path, result', [("IndiaStateCensusData - IndiaStateCensusData.txt", WrongFileType),
                                           ("IndiaStateCensusData - IndiaStateCensusData - WrongDelimter.csv",
                                            WrongDelimiterException),
